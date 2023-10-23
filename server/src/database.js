b49cc2883153709afa2e27f2db627f061767e604
markyupari <130168702+markyupari@users.noreply.github.com>
2023-10-22 20:34:48 -0500

    Update database.js

--- server/src/database.js
+++ server/src/database.js
@@ -3,8 +3,8 @@ mongoose.set("strictQuery", false); //added
 const User = require('./models/user.model');
 
 const connection = 'mongodb://mongo:27017/dockerize-me'; 
-//const connection = 'mongodb://localhost:27017/dockerize-me';
-//const connection = "mongodb+srv://yupari87157:OmksrBHo4DmIyfhw@cluster0.hgetl3b.mongodb.net/local_library?retryWrites=true&w=majority&appName=AtlasApp";
+/* Go to MongoDB Atlas on the web and follow the steps to create an login auth. Then replace your username and password in the below address */
+//const connection = "mongodb+srv://yourUserName:yourPassword@cluster0.hgetl3b.mongodb.net/local_library?retryWrites=true&w=majority&appName=AtlasApp";
 
 const connectDb = async () => {
   return await mongoose.connect(connection);
